f4e264763f8b7f23cfb75932d12fbc4b82ecba72
reinaldoper <reinaldoper83@gmail.com>
2024-04-28 13:03:29 -0400

    refactored: refactored file readme

--- workout_api/routers.py
+++ workout_api/routers.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from workout_api.atleta.controller import router as atletas
 from workout_api.categorias.controller import router as categorias
+from workout_api.centro_treinamento.controller import router as centro_treinamento
 
 
 api_router = APIRouter()
 api_router.include_router(atletas, prefix='/atletas', tags=['atletas'])
 api_router.include_router(categorias, prefix='/categorias', tags=['categorias'])
+api_router.include_router(centro_treinamento, prefix='/centro', tags=['centro-treinamento'])
